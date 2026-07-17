6a6568f683e8b10f39d5e3beb4380c2d802ea50b
gamaleyandrey-create <gamaleyandrey@gmail.com>
2026-07-17 21:53:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='panora-v21';
+const CACHE='panora-v22';
 const ASSETS=['./','index.html','styles.css','portal.css','quantity.css','account-state.css','cart-date.css','app.js','portal.js','admin.html','admin.css','admin.js','commerce.css','settings.css','export.css','commerce.js','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
