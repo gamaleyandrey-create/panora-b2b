b94c9a830b9ca9df9298e5e6d7fb0bc35f2eeea3
gamaleyandrey-create <gamaleyandrey@gmail.com>
2026-07-25 12:25:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='panora-v226';
+const CACHE='panora-v227';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil((async()=>{for(const key of await caches.keys())if(key!==CACHE)await caches.delete(key);await self.clients.claim()})()));
 self.addEventListener('fetch',event=>{
