793f31c4ba8ef30d86703e61ede5e222c60e5306
gamaleyandrey-create <gamaleyandrey@gmail.com>
2026-08-19 20:46:37 +0200

    Add files via upload

--- pwa-stable.js
+++ pwa-stable.js
@@ -1,7 +1,7 @@
 
-/* Panora 7.06 — stable install/update controller */
+/* Panora 7.07 — stable install/update controller */
 (function(){
-  const BUILD='7060';
+  const BUILD='7070';
   let deferredPrompt=null;
 
   function ensure(){
